58c860ef5bd9dc1b7a59becd1c4f2c145edeb304
vugluskr86 <ivashkin.vasiliy@gmail.com>
2018-08-31 23:38:03 +0500

    bump

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -1615,7 +1615,8 @@ share_type database::cashout_comment_helper( util::comment_reward_context& ctx, 
 
             author_tokens -= total_beneficiary;
 
-            auto sbd_steem     = ( author_tokens * comment.percent_steem_dollars ) / ( STEEMIT_100_PERCENT ) ;
+            // TODO: Check this
+            auto sbd_steem     = ( author_tokens * comment.percent_steem_dollars ) / ( STEEMIT_100_PERCENT * 2 ) ;
             auto vesting_steem = author_tokens - sbd_steem;
 
             const auto& author = get_account( comment.author );
